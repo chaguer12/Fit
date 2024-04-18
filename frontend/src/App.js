e00fbe9ecdf3ca3f39b43f222e7d8b9537c47f97
zoolotoui <chag.med.amine@gmail.com>
2024-04-18 16:38:00 +0100

    core: working on routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar.jsx';
+import Register from './components/Register.jsx';
 
 function App() {
   return (
     <div className="App">
 
      <Navbar/>
+     <Register/>
 
 
 
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import react from 'react'
+
+const Home = () =>{
+    return (
+        <>
+        <h1>hello world</h1>
+        </>
+
+    )
+}
